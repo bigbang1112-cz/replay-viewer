3212cdefb4525a82bf5f89ce76ddb3f7e4ae09e9
Petr Pivoňka <petrpiv1@gmail.com>
2023-02-19 03:46:05 +0100

    Add possible data inputs

--- ReplayViewer/ReplayViewerTool.cs
+++ ReplayViewer/ReplayViewerTool.cs
@@ -5,8 +5,18 @@ namespace ReplayViewer;
 
 public class ReplayViewerTool : MapViewerEngineTool
 {
-    public ReplayViewerTool(CGameCtnReplayRecord replay) : base(replay.Challenge ?? throw new Exception("No map found"))
+    private readonly CGameCtnChallenge map;
+    private readonly IEnumerable<CGameCtnGhost> ghosts;
+    private readonly CGameCtnReplayRecord? replay;
+
+    public ReplayViewerTool(CGameCtnChallenge map, IEnumerable<CGameCtnGhost> ghosts) : base(map)
     {
-        
+        this.map = map;
+        this.ghosts = ghosts;
+    }
+
+    public ReplayViewerTool(CGameCtnReplayRecord replay) : this(replay.Challenge ?? throw new Exception("No map found"), replay.GetGhosts())
+    {
+        this.replay = replay;
     }
 }
